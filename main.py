bb8ec0949c3bfb194a5d04ebff33c9d230a3d135
BlueWood1425 <nika.konyk@gmail.com>
2023-06-02 20:08:22 +0300

    Have done

---
+++ main.py
@@ -0,0 +1,16 @@
+try:
+    import neisnujuchijmodue
+    x = Shosoznachae()
+    x += y
+    l = list()
+    l.append(1)
+    print(l[5])
+    p = int("DESYAT'")
+except ImportError:
+    print("Помилка: Модуль `neisnujuchijmodue` не існує.")
+except NameError:
+    print("Помилка: Змінна `Shosoznachae` не визначена.")
+except IndexError:
+    print("Помилка!")
+except ValueError:
+    print("Помилка: Не ціле число.")
